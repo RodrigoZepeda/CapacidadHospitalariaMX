7854edf26b85ad6ade1b4b2434872321d7d90c19
RodrigoZepeda <rzepeda17@gmail.com>
2021-09-07 14:23:29 -0500

    Se actualizadon datos hasta 2021-09-07

--- scripts/descarga_municipal.py
+++ scripts/descarga_municipal.py
@@ -47,8 +47,8 @@ if datetime.strptime(descargar_desde,'%Y-%m-%d') <= datetime.strptime(descargar_
     
     
     folder_of_download = os.getcwd() + slash + "raw" + slash + "unidad_medica" 
-    sleep_time    = 15 #Tiempo que tarda la página de la UNAM de cambiar ventana
-    download_time = 2  #Tiempo que tarda en descargarse el archivo en tu red
+    sleep_time    = 60 #Tiempo que tarda la página de la UNAM de cambiar ventana
+    download_time = 30  #Tiempo que tarda en descargarse el archivo en tu red
     
     
     option = webdriver.ChromeOptions()
