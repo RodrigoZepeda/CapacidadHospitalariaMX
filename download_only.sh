8cb450d10a2ddd209638bb4f8936fb8ad53d13b0
RodrigoZepeda <rzepeda17@gmail.com>
2022-02-25 12:03:44 -0600

    Actualización 2022-02-25

--- download_only.sh
+++ download_only.sh
@@ -5,6 +5,8 @@
 #Author: Rodrigo Zepeda
 #Contact: rzepeda17[at]gmail.com
 #----------------------------------------
+. ~/.keychain/`/bin/hostname`-sh
+cd /home/rodrigo/CapacidadHospitalariaMX
 date=$(date '+%Y-%m-%d')
 /home/rodrigo/miniconda3/envs/CapacidadHospitalaria/bin/python3 /home/rodrigo/CapacidadHospitalariaMX/scripts/descarga_estatal.py
 /home/rodrigo/miniconda3/envs/CapacidadHospitalaria/bin/python3 /home/rodrigo/CapacidadHospitalariaMX/scripts/descarga_municipal.py
