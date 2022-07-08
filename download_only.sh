40a654df5c598ca224706f3f0c30c3e23c4c7061
Rodrigo Zepeda-Tello <rzepeda17@gmail.com>
2022-07-08 16:36:07 -0500

    Actualización 2022-07-08

--- download_only.sh
+++ download_only.sh
@@ -7,9 +7,9 @@
 . ~/.keychain/`/bin/hostname`-sh
 cd /home/rodrigo/CapacidadHospitalariaMX
 date=$(date '+%Y-%m-%d')
-/home/rodrigo/miniconda3/envs/CapacidadHospitalaria/bin/python3 /home/rodrigo/CapacidadHospitalariaMX/scripts/descarga_estatal.py
-/home/rodrigo/miniconda3/envs/CapacidadHospitalaria/bin/python3 /home/rodrigo/CapacidadHospitalariaMX/scripts/descarga_municipal.py
-/usr/bin/R < /home/rodrigo/CapacidadHospitalariaMX/scripts/genera_base_unica.R --no-save
-/usr/bin/git -C /home/rodrigo/CapacidadHospitalariaMX add .
-/usr/bin/git -C /home/rodrigo/CapacidadHospitalariaMX commit -m "Actualización ${date}"
-/usr/bin/git -C /home/rodrigo/CapacidadHospitalariaMX push origin master
+/home/rodrigo/miniconda3/envs/CapacidadHospitalaria/bin/python3 /media/rodrigo/covid/CapacidadHospitalariaMX/scripts/descarga_estatal.py
+/home/rodrigo/miniconda3/envs/CapacidadHospitalaria/bin/python3 /media/rodrigo/covid/CapacidadHospitalariaMX/scripts/descarga_municipal.py
+/usr/bin/R < /media/rodrigo/covid/CapacidadHospitalariaMX/scripts/genera_base_unica.R --no-save
+/usr/bin/git -C /media/rodrigo/covid/CapacidadHospitalariaMX add .
+/usr/bin/git -C /media/rodrigo/covid/CapacidadHospitalariaMX commit -m "Actualización ${date}"
+/usr/bin/git -C /media/rodrigo/covid/CapacidadHospitalariaMX push origin master
