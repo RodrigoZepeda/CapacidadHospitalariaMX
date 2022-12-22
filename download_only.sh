8a9f094b94e1a7595c923fff9bb98340b03b9ac7
RodZepeda <rzepeda17@gmail.com>
2022-12-22 12:43:01 -0600

    Actualización automática de datos al

--- download_only.sh
+++ download_only.sh
@@ -5,11 +5,11 @@
 #Contact: rzepeda17[at]gmail.com
 #----------------------------------------
 #. ~/.keychain/`/bin/hostname`-sh
-cd /home/rod/CapacidadHospitalariaMX
-date=$(date '+%Y-%m-%d')
-/home/rod/miniconda3/envs/CapacidadHospitalariaMx/bin/python3 /home/rod/CapacidadHospitalariaMX/scripts/descarga_estatal.py
-/home/rod/miniconda3/envs/CapacidadHospitalariaMx/bin/python3 /home/rod/CapacidadHospitalariaMX/scripts/descarga_municipal.py
-/usr/bin/R < /home/rod/CapacidadHospitalariaMX/scripts/genera_base_unica.R --no-save
+#cd /home/rod/CapacidadHospitalariaMX
+#date=$(date '+%Y-%m-%d')
+#/home/rod/miniconda3/envs/CapacidadHospitalariaMx/bin/python3 /home/rod/CapacidadHospitalariaMX/scripts/descarga_estatal.py
+#/home/rod/miniconda3/envs/CapacidadHospitalariaMx/bin/python3 /home/rod/CapacidadHospitalariaMX/scripts/descarga_municipal.py
+#/usr/bin/R < /home/rod/CapacidadHospitalariaMX/scripts/genera_base_unica.R --no-save
 /usr/bin/git -C /home/rod/CapacidadHospitalariaMX add .
 /usr/bin/git -C /home/rod/CapacidadHospitalariaMX commit -m "Actualización automática de datos al ${date}"
 /usr/bin/git -C /home/rod/CapacidadHospitalariaMX push origin master
